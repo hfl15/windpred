aefd437ffde11af4dcf4b69e6acdf9efaeb46174
黄凡玲 <fanlinghuang@163.com>
2020-09-02 20:13:42 +0800

    update

--- windpred/expslid/base_lstm.py
+++ windpred/expslid/base_lstm.py
@@ -3,17 +3,25 @@ import os
 from windpred.utils.base import tag_path
 
 from windpred.utils.model_base import DefaultConfig, BaseLSTM
-from windpred.baseline.temporal_nn import main
+from windpred.utils import exp_dir
+from windpred.baseline import temporal_nn
 from windpred.expslid.base import eval_mode
 
 
 if __name__ == '__main__':
     tag = tag_path(os.path.abspath(__file__), 2)
 
-    target = 'SPD10'
+    target = 'DIR10'
     mode_list = ['run-history', 'run-future', 'run-history_future']
-    for mode in mode_list:
-        main(tag, DefaultConfig, target, mode, eval_mode, BaseLSTM)
+
+    if target == 'DIR10':
+        for mode in mode_list:
+            tag_file_list = [None]
+            exp_dir.main('run', eval_mode, tag, tag_file_list)
+            exp_dir.main('reduce', eval_mode, tag, tag_file_list)
+    else:
+        for mode in mode_list:
+            temporal_nn.main(tag, DefaultConfig, target, mode, eval_mode, BaseLSTM)
 
 
 
--- windpred/expslid/mhstn_dir.py
+++ windpred/expslid/mhstn_dir.py
@@ -1,10 +1,13 @@
-from windpred.mhstn.mhstn import get_tags
+import os
+
+from windpred.utils.base import path_split
 from windpred.utils.exp_dir import main
+from windpred.mhstn.mhstn import get_tags
 from windpred.expslid.base import eval_mode
 
 
 if __name__ == '__main__':
-    file_exp_in = 'expslid_mhstn'  # !!! according to the eval_mode
+    file_exp_in = '{}_mhstn'.format(path_split(os.path.abspath(__file__))[-2])
     tag_file_list = get_tags()
 
     main('run', eval_mode, file_exp_in, tag_file_list)
