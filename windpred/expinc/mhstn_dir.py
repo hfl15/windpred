2e294058153cd2979f7169292b9b8479c12311df
黄凡玲 <fanlinghuang@163.com>
2020-09-02 17:02:19 +0800

    update

--- windpred/expinc/mhstn_dir.py
+++ windpred/expinc/mhstn_dir.py
@@ -1,10 +1,10 @@
 from windpred.mhstn.mhstn import get_tags
 from windpred.utils.exp_dir import main
-from windpred.expslid.base import eval_mode
+from windpred.expinc.base import eval_mode
 
 
 if __name__ == '__main__':
-    file_exp_in = 'expslid_mhstn'  # !!! according to the eval_mode
+    file_exp_in = 'expinc_mhstn'  # !!! according to the eval_mode
     tag_file_list = get_tags()
 
     main('run', eval_mode, file_exp_in, tag_file_list)
