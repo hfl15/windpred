e186fa96af938c5f12af0d9a18729b4cdad302e4
黄凡玲 <fanlinghuang@163.com>
2020-09-03 14:59:36 +0800

    update

---
+++ windpred/expinc/base_mlp.py
@@ -0,0 +1,33 @@
+import os
+
+from windpred.utils.base import tag_path
+
+from windpred.utils.model_base import DefaultConfig, BaseMLP
+from windpred.utils import exp_dir
+from windpred.baseline import temporal_nn
+
+from windpred.expinc.base import eval_mode
+
+if __name__ == '__main__':
+    tag = tag_path(os.path.abspath(__file__), 2)
+
+    target = 'SPD10'
+    feature_mode_list = ['history', 'future', 'history_future']
+
+    if target == 'DIR10':
+        for mode in feature_mode_list:
+            tag_file_list = [None]
+            file_exp_in = os.path.join(tag, mode)
+            exp_dir.main('run', eval_mode, file_exp_in, tag_file_list)
+            exp_dir.main('reduce', eval_mode, file_exp_in, tag_file_list)
+    else:
+        mode = 'run'
+        for feature_mode in feature_mode_list:
+            temporal_nn.main(tag, DefaultConfig, target, mode+'-'+feature_mode, eval_mode, BaseMLP)
+
+
+
+
+
+
+
