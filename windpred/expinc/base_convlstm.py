897002d2f619216c8d68f318c288698bd6f09d9d
黄凡玲 <fanlinghuang@163.com>
2020-09-03 15:07:48 +0800

    update

---
+++ windpred/expinc/base_convlstm.py
@@ -0,0 +1,29 @@
+import os
+
+from windpred.utils.base import tag_path
+from windpred.utils.model_base import DefaultConfig
+
+from windpred.baseline import convlstm
+from windpred.utils import exp_dir
+
+from windpred.expinc.base import eval_mode
+
+if __name__ == '__main__':
+    tag = tag_path(os.path.abspath(__file__), 2)
+
+    target = 'SPD10'
+    model_name = 'convlstm'
+
+    if target == 'DIR10':
+        tag_file_list = [model_name]
+        exp_dir.main('run', eval_mode, tag, tag_file_list)
+        exp_dir.main('reduce', eval_mode, tag, tag_file_list)
+    else:
+        features_history, features_future = [target], ['NEXT_NWP_{}'.format(target)]
+        mode = 'run'
+        convlstm.main(target, mode, eval_mode, DefaultConfig, tag, model_name, features_history, features_future)
+
+
+
+
+
