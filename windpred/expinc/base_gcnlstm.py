6c2480321375c6637bb08ba85cb64b14aa971a64
黄凡玲 <fanlinghuang@163.com>
2020-09-03 15:03:01 +0800

    update

---
+++ windpred/expinc/base_gcnlstm.py
@@ -0,0 +1,27 @@
+import os
+
+from windpred.utils.base import tag_path
+from windpred.utils.model_base import DefaultConfig
+
+from windpred.baseline import gcn
+from windpred.utils import exp_dir
+
+from windpred.expinc.base import eval_mode
+
+
+if __name__ == '__main__':
+    tag = tag_path(os.path.abspath(__file__), 2)
+
+    target = 'SPD10'
+    model_name = 'gcn_seq_lstm_seq'
+
+    if target == 'DIR10':
+        tag_file_list = [model_name]
+        exp_dir.main('run', eval_mode, tag, tag_file_list)
+        exp_dir.main('reduce', eval_mode, tag, tag_file_list)
+    else:
+        features_history, features_future = [target], ['NEXT_NWP_{}'.format(target)]
+        mode = 'run'
+        adjacency_norm = 'localpooling_filter'
+        gcn.main(target, mode, eval_mode, DefaultConfig, tag, model_name, features_history, features_future, adjacency_norm)
+
