b9e99f6863c959cc16ce395cecef66234cccf258
黄凡玲 <fanlinghuang@163.com>
2020-09-05 15:31:20 +0800

    update

--- windpred/expinc/mhstn_covar_all.py
+++ windpred/expinc/mhstn_covar_all.py
@@ -13,7 +13,7 @@ from windpred.expinc.base import eval_mode
 if __name__ == '__main__':
     tag = tag_path(os.path.abspath(__file__), 2)
 
-    target = 'DIR10'
+    target = 'SPD10'
     if target == 'DIR10':
         tag_file_list = mhstn.get_tags()
         exp_dir.main('run', eval_mode, tag, tag_file_list)
