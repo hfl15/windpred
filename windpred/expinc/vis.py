bbab6b754d1a6294e53936aa6a9fe9e5ae174c74
黄凡玲 <fanlinghuang@163.com>
2020-09-08 15:23:41 +0800

    update

--- windpred/expinc/vis.py
+++ windpred/expinc/vis.py
@@ -40,13 +40,13 @@ if __name__ == '__main__':
         plt.plot(obs, label='TRUTH')
         plt.plot(nwp, label='NWP')
 
-        # lstm_h_path = 'expinc_base_lstm/history/{}/{}/{}/y_pred_{}.txt'.format(target, month, str(i_run), station_name)
-        # lstm_h_pred = np.loadtxt(os.path.join(DIR_LOG, lstm_h_path))
-        # plt.plot(lstm_h_pred, label='LSTM(h)')
+        lstm_h_path = 'expinc_base_lstm/history/{}/{}/{}/y_pred_{}.txt'.format(target, month, str(i_run), station_name)
+        lstm_h_pred = np.loadtxt(os.path.join(DIR_LOG, lstm_h_path))
+        plt.plot(lstm_h_pred, label='LSTM(h)')
 
-        # fcn_f_path = 'expinc_base_mlp/future/{}/{}/{}/y_pred_{}.txt'.format(target, month, str(i_run), station_name)
-        # fcn_f_pred = np.loadtxt(os.path.join(DIR_LOG, fcn_f_path))
-        # plt.plot(fcn_f_pred, label='MLP(f)')
+        fcn_f_path = 'expinc_base_mlp/future/{}/{}/{}/y_pred_{}.txt'.format(target, month, str(i_run), station_name)
+        fcn_f_pred = np.loadtxt(os.path.join(DIR_LOG, fcn_f_path))
+        plt.plot(fcn_f_pred, label='MLP(f)')
 
         frame_cnn_covar_path = 'expinc_mhstn_covar/{}/{}/{}/y_pred_{}_combine_module_conv.txt'.format(
             target, month, str(i_run), station_name)
