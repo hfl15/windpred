79513e2b11503c594c320603307b6f485941cfbe
黄凡玲 <fanlinghuang@163.com>
2021-01-06 08:30:11 +0800

    update

---
+++ windpred/exproll/base_convention.py
@@ -0,0 +1,17 @@
+import os
+
+from windpred.utils.base import tag_path
+from windpred.utils.model_base import DefaultConfig
+
+from windpred.baseline.convention import main, MODELS
+
+from windpred.exproll.base import eval_mode
+
+
+if __name__ == '__main__':
+    tag = tag_path(os.path.abspath(__file__), 2)
+    mode = 'run'
+    target = 'V'
+    for model_name in MODELS.keys():
+        main(tag, DefaultConfig(), target, mode, eval_mode, model_name)
+
