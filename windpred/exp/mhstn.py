d57ffb3265a1843c122b92e8fddeb31a66836b53
黄凡玲 <fanlinghuang@163.com>
2021-01-11 05:59:27 +0800

    update

--- windpred/exp/mhstn.py
+++ windpred/exp/mhstn.py
@@ -1,24 +1,71 @@
 from windpred.utils.model_base import DefaultConfig
+from windpred.utils.exp import get_covariates_history_all, get_covariates_future_all
 from windpred.utils import exp_dir
 
-from windpred.mhstn.base import CSV_RESULT_FILES
+from windpred.mhstn.base import CSV_RESULT_FILES, get_covariates_history
 from windpred.mhstn import mhstn
 
 
-def run(target, tag, eval_mode):
+def _run(target, tag, eval_mode, features_history, features_future):
     if target == 'DIR':
         tag_file_list = mhstn.get_tags()
         for mode in ['run', 'reduce']:
             exp_dir.main(mode, DefaultConfig(), eval_mode, tag, tag_file_list)
     else:
-        features_history, features_future = [target], ['NEXT_NWP_{}'.format(target)]
         mode_list = ['temporal', 'spatial-conv', 'combine-conv', 'reduce']
         for mode in mode_list:
             csv_result_list = CSV_RESULT_FILES
             mhstn.main(target, mode, eval_mode, DefaultConfig, tag, features_history, features_future, csv_result_list)
 
 
+def run(target, tag, eval_mode):
+    features_history, features_future = [target], ['NEXT_NWP_{}'.format(target)]
+    _run(target, tag, eval_mode, features_history, features_future)
+    # if target == 'DIR':
+    #     tag_file_list = mhstn.get_tags()
+    #     for mode in ['run', 'reduce']:
+    #         exp_dir.main(mode, DefaultConfig(), eval_mode, tag, tag_file_list)
+    # else:
+    #     features_history, features_future = [target], ['NEXT_NWP_{}'.format(target)]
+    #     mode_list = ['temporal', 'spatial-conv', 'combine-conv', 'reduce']
+    #     for mode in mode_list:
+    #         csv_result_list = CSV_RESULT_FILES
+    #         mhstn.main(target, mode, eval_mode, DefaultConfig, tag, features_history, features_future, csv_result_list)
 
+
+def run_covar(target, tag, eval_mode):
+    features_history = get_covariates_history(target)
+    features_future = ['NEXT_NWP_{}'.format(target)]
+    _run(target, tag, eval_mode, features_history, features_future)
+    # if target == 'DIR':
+    #     tag_file_list = mhstn.get_tags()
+    #     exp_dir.main('run', eval_mode, tag, tag_file_list)
+    #     exp_dir.main_old('reduce', eval_mode, tag, tag_file_list)
+    # else:
+    #     features_history = get_covariates_history(target)
+    #     features_future = ['NEXT_NWP_{}'.format(target)]
+    #     mode_list = ['temporal', 'spatial-conv', 'combine-conv', 'reduce']
+    #     for mode in mode_list:
+    #         csv_result_list = CSV_RESULT_FILES
+    #         mhstn.main(target, mode, eval_mode, DefaultConfig, tag, features_history, features_future,
+    #                    csv_result_list)
+
+
+def run_covar_all(target, tag, eval_mode):
+    features_history = get_covariates_history_all()
+    features_future = get_covariates_future_all()
+    _run(target, tag, eval_mode, features_history, features_future)
+    # if target == 'DIR':
+    #     tag_file_list = mhstn.get_tags()
+    #     exp_dir.main_old('run', eval_mode, tag, tag_file_list)
+    #     exp_dir.main_old('reduce', eval_mode, tag, tag_file_list)
+    # else:
+    #     features_history = get_covariates_history_all()
+    #     features_future = get_covariates_future_all()
+    #     mode_list = ['temporal', 'spatial-conv', 'combine-conv', 'reduce']
+    #     for mode in mode_list:
+    #         csv_result_list = CSV_RESULT_FILES
+    #         mhstn.main(target, mode, eval_mode, DefaultConfig, tag, features_history, features_future, csv_result_list)
 
 
 
--- windpred/expinc/mhstn.py
+++ windpred/expinc/mhstn.py
@@ -1,12 +1,3 @@
-import os
-
-from windpred.utils.base import tag_path
-from windpred.utils.model_base import DefaultConfig
-from windpred.utils import exp_dir
-
-from windpred.mhstn.base import CSV_RESULT_FILES
-from windpred.mhstn import mhstn
-
 from windpred.exp.mhstn import run
 
 from windpred.expinc.base import *
@@ -15,22 +6,6 @@ if __name__ == '__main__':
     tag = get_tag(__file__)
     target = 'V'
     run(target, tag, eval_mode)
-
-# if __name__ == '__main__':
-#     tag = tag_path(os.path.abspath(__file__), 2)
-#
-#     target = 'V'
-#
-#     if target == 'DIR':
-#         tag_file_list = mhstn.get_tags()
-#         exp_dir.main_old('run', eval_mode, tag, tag_file_list)
-#         exp_dir.main_old('reduce', eval_mode, tag, tag_file_list)
-#     else:
-#         features_history, features_future = [target], ['NEXT_NWP_{}'.format(target)]
-#         mode_list = ['temporal', 'spatial-conv', 'combine-conv', 'reduce']
-#         for mode in mode_list:
-#             csv_result_list = CSV_RESULT_FILES
-#             mhstn.main(target, mode, eval_mode, DefaultConfig, tag, features_history, features_future, csv_result_list)
 
 
 
