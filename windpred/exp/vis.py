e7b1285a70f94b4adc47dd9a3c4261f05a03a107
黄凡玲 <fanlinghuang@163.com>
2021-01-22 08:00:14 +0800

    update

--- windpred/exp/vis.py
+++ windpred/exp/vis.py
@@ -2,12 +2,56 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-from windpred.utils.base import tag_path, make_dir, DIR_LOG
+from windpred.utils.base import make_dir, DIR_LOG
 from windpred.utils.evaluation import cal_delta
 from windpred.utils.data_parser import DataGenerator
-from windpred.utils.model_base import DefaultConfig, MONTH_LIST
+from windpred.utils.model_base import MONTH_LIST
 
 from matplotlib.backends.backend_pdf import PdfPages
+
+
+def plot(tag, config, target, mhstn_root, lstm_root=None):
+    i_run = 0
+    month = str(MONTH_LIST[-1])
+    dir_log = make_dir(os.path.join(DIR_LOG, tag, target, str(i_run)))
+
+    data_generator_list = []
+    for obs_data_path in config.obs_data_path_list:
+        data_generator = DataGenerator(config.period, config.window, path=obs_data_path)
+        data_generator.prepare_data(config.target_size, train_step=config.train_step, test_step=config.test_step,
+                                    single_step=config.single_step)
+        data_generator_list.append(data_generator)
+
+    for data_generator in data_generator_list:
+        station_name = data_generator.station_name
+
+        _, nwp, obs, _ = data_generator.extract_evaluation_data(target)
+        plt.plot(obs, label='TRUTH')
+        plt.plot(nwp, label='NWP')
+
+        if lstm_root is not None:
+            lstm_h_path = '{}/history/{}/{}/{}/y_pred_{}.txt'.format(
+                lstm_root, target, month, str(i_run), station_name)
+            lstm_h_pred = np.loadtxt(os.path.join(DIR_LOG, lstm_h_path))
+            plt.plot(lstm_h_pred, label='LSTM(h)')
+
+        mhstn_path = '{}/{}/{}/{}/y_pred_{}_combine_module_conv.txt'.format(
+            mhstn_root, target, month, str(i_run), station_name)
+        mhstn_pred = np.loadtxt(os.path.join(DIR_LOG, mhstn_path))
+        plt.plot(mhstn_pred, label='MHSTN')
+
+        plt.legend(loc='best')
+        plt.ylabel('Value (meter/second)')
+        plt.xlabel('Time (hours)')
+        plt.tight_layout()
+
+        plt.savefig(os.path.join(dir_log, "{}".format(station_name)), dpi=750, bbox_inches='tight')
+
+        pdf = PdfPages(os.path.join(dir_log, "{}.pdf".format(station_name)))
+        pdf.savefig()
+        pdf.close()
+
+        plt.close()
 
 
 def plot_dir(tag, config, target, mhstn_root):
@@ -57,15 +101,6 @@ def plot_dir(tag, config, target, mhstn_root):
         pdf.close()
 
         plt.close()
-
-
-if __name__ == '__main__':
-    tag = tag_path(os.path.abspath(__file__), 2)
-    config = DefaultConfig()
-
-    target = 'DIR10'
-
-
 
 
 
--- windpred/expinc/vis_with_lstm_h.py
+++ windpred/expinc/vis_with_lstm_h.py
@@ -2,13 +2,12 @@ from windpred.utils.model_base import DefaultConfig
 from windpred.exp.vis import plot, plot_dir
 from windpred.expinc.base import *
 
-
 if __name__ == '__main__':
     tag = get_tag(__file__)
 
-    plot(tag, DefaultConfig(), 'V', 'expinc_mhstn_covar')
-    plot(tag, DefaultConfig(), 'VX', 'expinc_mhstn')
-    plot(tag, DefaultConfig(), 'VY', 'expinc_mhstn')
+    plot(tag, DefaultConfig(), 'V', 'expinc_mhstn_covar', 'expinc_base_lstm')
+    plot(tag, DefaultConfig(), 'VX', 'expinc_mhstn', 'expinc_base_lstm')
+    plot(tag, DefaultConfig(), 'VY', 'expinc_mhstn', 'expinc_base_lstm')
     plot_dir(tag, DefaultConfig(), 'DIR', 'expinc_mhstn_covar')
 
 
