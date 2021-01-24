39bceabf18389218a42ecbdb0b91035f62eee08e
黄凡玲 <fanlinghuang@163.com>
2021-01-24 17:47:49 +0800

    update

--- windpred/expinc/base_convention_spatial.py
+++
@@ -1,8 +0,0 @@
-from windpred.exp.base_convention import run_spatial
-from windpred.expinc.base import *
-
-if __name__ == '__main__':
-    tag = get_tag(__file__)
-    target = 'V'
-    run_spatial(target, tag, eval_mode)
-
